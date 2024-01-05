fe65c5d32e3d697f9a42690aea1d5a86139c9387
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-01-05 22:51:38 +0200

    made some updates for two sections

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+    var menu = document.getElementById("myDiv");
+    menu.classList.toggle("show-menu");
+  }
+  function closeMenu() {
+    var menu = document.getElementById("myDiv");
+    menu.classList.remove("show-menu");
+  }  
