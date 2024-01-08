62461b49d718ef4a916b7b66df7b48dc480475c1
OleksiyBodnia <90392804+OleksiyBodnia@users.noreply.github.com>
2024-01-08 17:15:21 +0200

    made last sections

--- script.js
+++ script.js
@@ -6,3 +6,26 @@ function toggleMenu() {
     var menu = document.getElementById("myDiv");
     menu.classList.remove("show-menu");
   }  
+
+  const showFormBtn = document.getElementById('showFormBtn');
+  const overlay = document.getElementById('overlay');
+  const formContainer = document.getElementById('formContainer');
+  const closeButton = document.getElementById('closeButton'); // Додайте id кнопки закриття
+  
+  showFormBtn.addEventListener('click', function() {
+    overlay.style.display = 'block';
+    formContainer.style.display = 'block';
+  });
+  
+  overlay.addEventListener('click', function(event) {
+    if (event.target === overlay) {
+      overlay.style.display = 'none';
+      formContainer.style.display = 'none';
+    }
+  });
+  
+  closeButton.addEventListener('click', function() {
+    overlay.style.display = 'none';
+    formContainer.style.display = 'none';
+  });
+  
